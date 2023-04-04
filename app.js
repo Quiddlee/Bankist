50c00a48166215c50e379980677a484e57e1bf51
Bogdan <thegarrybit@gmail.com>
2023-04-04 17:44:10 +0300

    refactor: code quality improve

--- app.js
+++ app.js
@@ -6,33 +6,70 @@
 
 // Data
 const account1 = {
-  login: 'js',
   owner: 'Jonas Schmedtmann',
   currency: 'EUR',
   movements: [ 200, 450, -400, 3000, -650, -130, 70, 1300 ],
+  movementsDates: [
+    '2019-01-28T09:15:04.904Z',
+    '2019-04-01T10:17:24.185Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2021-11-18T21:31:17.178Z',
+    '2022-12-23T07:42:02.383Z',
+    '2022-03-08T14:11:59.604Z',
+    '2023-03-12T10:51:36.790Z'
+  ],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
-  login: 'jd',
   owner: 'Jessica Davis',
   currency: 'USD',
   movements: [ 5000, 3400, -150, -790, -3210, -1000, 8500, -30 ],
+  movementsDates: [
+    '2022-01-25T14:18:46.235Z',
+    '2022-02-05T16:33:06.386Z',
+    '2022-03-10T14:43:26.374Z',
+    '2022-04-25T18:49:59.371Z',
+    '2022-11-01T13:15:33.035Z',
+    '2022-11-30T09:48:16.867Z',
+    '2022-12-25T06:04:23.907Z',
+    '2023-02-26T12:01:20.894Z',
+  ],
   interestRate: 1.5,
   pin: 2222,
 };
 
 const account3 = {
   owner: 'Steven Thomas Williams',
+  currency: 'GBP',
   movements: [ 200, -200, 340, -300, -20, 50, 400, -460 ],
+  movementsDates: [
+    '2018-06-20T18:49:59.371Z',
+    '2018-09-10T13:15:33.035Z',
+    '2019-07-22T09:48:16.867Z',
+    '2019-08-19T06:04:23.907Z',
+    '2020-04-25T12:01:20.894Z',
+    '2021-07-09T12:01:20.894Z',
+    '2022-01-20T12:01:20.894Z',
+    '2023-03-17T12:01:20.894Z',
+  ],
   interestRate: 0.7,
   pin: 3333,
 };
 
 const account4 = {
   owner: 'Sarah Smith',
+  currency: 'UAH',
   movements: [ 430, 1000, 700, 50, 90 ],
+  movementsDates: [
+    '2022-06-02T18:49:59.371Z',
+    '2022-09-01T13:15:33.035Z',
+    '2023-10-23T09:48:16.867Z',
+    '2023-12-26T06:04:23.907Z',
+    '2023-02-21T12:01:20.894Z',
+  ],
   interestRate: 1,
   pin: 4444,
 };
@@ -68,13 +105,27 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // App
 const currencies = new Map([
   [ 'EUR', '€' ],
-  [ 'USD', '$' ]
+  [ 'USD', '$' ],
+  [ 'GBP', '£' ],
+  [ 'UAH', '₴' ],
 ]);
 let logOutTimerId, sortMovementsBounded, transferMoneyBounded,
   requestLoanBounded, closeAccountBounded;
 
-const formatNumberForEuro = (num) => num.toFixed(2).replace('.', ',');
-const formatNumberBelowTen = (num) => num >= 10 ? num : `0${ num }`;
+const formatNumberForEuro = num => num.toFixed(2).replace('.', ',');
+const formatNumberBelowTen = num => num >= 10 ? num : `0${ num }`;
+const resetForm = formClass => document.getElementsByClassName(
+  formClass)[0].reset();
+
+const createUsernames = accs => {
+  accs.forEach(acc => {
+    acc.login = acc.owner.
+      toLowerCase().
+      split(' ').
+      map(word => word.at(0)).
+      join('');
+  });
+};
 
 const validateAndGetUser = () => {
   let isValid = false;
@@ -85,15 +136,14 @@ const validateAndGetUser = () => {
 
   for (const account of accounts) {
     const { pin, login } = account;
+    if (login !== inputLogin && pin !== +inputPin) continue;
 
-    if (login === inputLogin && pin === +inputPin) {
-      isValid = true;
-      validUser = account;
-      break;
-    }
+    isValid = true;
+    validUser = account;
+    break;
   }
 
-  document.getElementsByClassName('login')[0].reset();
+  resetForm('login');
   return { isValid, validUser };
 };
 
@@ -123,12 +173,13 @@ const logOutUser = () => {
 
 const getCurrentDate = () => {
   const date = new Date();
+  const year = date.getFullYear();
+  const month = formatNumberBelowTen(date.getMonth() + 1);
+  const day = formatNumberBelowTen(date.getDate());
   const time = `${ date.getHours() }:${ formatNumberBelowTen(
     date.getMinutes()) }`;
-  const day = formatNumberBelowTen(date.getDate());
-  const month = formatNumberBelowTen(date.getMonth() + 1);
 
-  return `${ day }/${ month }/${ date.getFullYear() }, ${ time }`;
+  return `${ day }/${ month }/${ year }, ${ time }`;
 };
 
 const renderUIComponent = (selectorOrElement, content) => {
@@ -142,27 +193,36 @@ const renderUIComponent = (selectorOrElement, content) => {
 
 const renderBalance = (user, currencySign) => {
   labelBalance.textContent = `${ formatNumberForEuro(user.movements.reduce(
-    (curr, prev) => curr + prev)) } ${ currencySign }`;
+    (acc, curr) => acc + curr)) } ${ currencySign }`;
 };
 
-const renderMovements = (movements, currencySign) => {
+const formatMovementDate = (date) => {
+  return date?.slice(0, date?.
+      indexOf('T'))?.
+      split('-')?.
+      reverse()?.
+      join('/')
+    ||
+    getCurrentDate().
+      slice(0, 11);
+};
+
+const renderMovements = (user, currencySign) => {
   containerMovements.innerHTML = '';
+  const { movements, movementsDates } = user;
 
   movements.forEach((movement, i) => {
     const moveEl = document.createElement('div');
-    const depositClass = movement >= 0
-      ? 'movements__type--deposit'
-      : 'movements__type--withdrawal';
+    const depositClass = movement >= 0 ? 'deposit' : 'withdrawal';
     const depositType = depositClass.slice(depositClass.lastIndexOf('-') + 1);
-    const moveDate = getCurrentDate().
-      slice(0, getCurrentDate().indexOf(','));
+    const currMovementDate = formatMovementDate(movementsDates[i]);
 
     moveEl.classList.add('movements__row');
     moveEl.innerHTML = `
-          <div class='movements__type ${ depositClass } '>
+          <div class='movements__type movements__type--${ depositClass } '>
             ${ i + 1 } ${ depositType }
           </div>
-          <div class='movements__date'>${ moveDate }</div>
+          <div class='movements__date'>${ currMovementDate }</div>
           <div class='movements__value'>
             ${ formatNumberForEuro(movement) } ${ currencySign }
           </div>
@@ -185,7 +245,7 @@ const renderSummary = (user, currencySign) => {
     [ labelSumIn, summIn ],
     [ labelSumOut, summOut ],
   ].forEach(([ label, summ ]) => {
-    const calculatedSumm = summ.reduce((curr, prev) => curr + prev);
+    const calculatedSumm = summ.reduce((curr, prev) => curr + prev, 0);
 
     label.textContent = `${ formatNumberForEuro(
       Math.abs(calculatedSumm)) } ${ currencySign }`;
@@ -195,17 +255,18 @@ const renderSummary = (user, currencySign) => {
     interestSum) } ${ currencySign }`;
 };
 
-const sortMovements = (movements, currencySign) => {
-  console.log(movements);
-  for (let i = 1; i < movements.length; i++) {
-    if (movements.at(i - 1) > movements.at(i)) {
-      return renderMovements(movements.sort((prev, curr) => prev - curr),
-        currencySign);
-    }
+const sortMovements = (user, currencySign) => {
+  const { movements } = user;
+
+  for (let i = movements.length; i > 0; --i) {
+    if (!(movements.at(i + 1) < movements.at(i))) continue;
+
+    movements.sort((prev, curr) => prev - curr);
+    return renderMovements(user, currencySign);
   }
 
-  return renderMovements(movements.sort((prev, curr) => prev + curr),
-    currencySign);
+  movements.sort((prev, curr) => prev + curr);
+  renderMovements(user, currencySign);
 };
 
 const setLogOutTimer = () => {
@@ -220,6 +281,7 @@ const setLogOutTimer = () => {
 
   logOutTimerId = setInterval(() => {
     seconds && seconds--;
+
     if (minutes === 0 && seconds === 0) {
       logOutUser();
       clearTimeout(logOutTimerId);
@@ -241,34 +303,38 @@ const transferMoney = (user, currUserCurrencySign, event) => {
   const transferTo = inputTransferTo.value;
   const transferAmount = +inputTransferAmount.value;
   const currUserMovements = movements;
-  document.getElementsByClassName('form--transfer')[0].reset();
+  resetForm('form--transfer');
 
   for (const { login, movements: recipientMovements } of accounts) {
-    if (login === transferTo) {
-      currUserMovements.push(-transferAmount);
-      recipientMovements.push(transferAmount);
-      renderMovements(movements, currUserCurrencySign);
-      renderSummary(user, currUserCurrencySign);
-      renderBalance(user, currUserCurrencySign);
-      return;
-    }
+    if (login !== transferTo) continue;
+
+    currUserMovements.push(-transferAmount);
+    recipientMovements.push(transferAmount);
+    renderMovementsSummaryAndBalance(user, currUserCurrencySign);
+    return;
   }
+};
+
+const renderMovementsSummaryAndBalance = (user, currencySign) => {
+  [ renderMovements, renderSummary, renderBalance ].forEach(
+    func => func(user, currencySign));
 };
 
 const requestLoan = (user, currencySign, event) => {
   event.preventDefault();
-  const amount = +inputLoanAmount.value;
-  inputLoanAmount.value = '';
-  const { movements } = user;
 
-  if (movements.some(move => move > (amount * 10) / 100) && amount > 0) {
-    setTimeout(() => {
-      movements.push(amount);
-      renderMovements(movements, currencySign);
-      renderSummary(user, currencySign);
-      renderBalance(user, currencySign);
-    }, Math.trunc(((Math.random() * 2) + 1) * 1000));
-  }
+  const amount = +inputLoanAmount.value;
+  const { movements } = user;
+  inputLoanAmount.value = '';
+
+  if (
+    !(movements.some(
+      move => move > (amount * 10) / 100) && amount > 0)
+  ) return;
+  setTimeout(() => {
+    movements.push(amount);
+    renderMovementsSummaryAndBalance(user, currencySign);
+  }, Math.trunc(((Math.random() * 2) + 1) * 1000));
 };
 
 const closeAccount = (user, event) => {
@@ -289,38 +355,41 @@ const closeAccount = (user, event) => {
   }
 };
 
+const initializeApp = (user, currencySign) => {
+  const { owner } = user;
+  containerApp.style.opacity = '100%';
+
+  setLogOutTimer();
+  renderWelcomeMessage(owner);
+  [ renderBalance, renderMovements, renderSummary ].forEach(
+    func => func(user, currencySign));
+};
+
 btnLogin.addEventListener('click', event => {
   event.preventDefault();
   logOutTimerId && clearInterval(logOutTimerId);
+  createUsernames(accounts);
 
   const {
     isValid,
     validUser,
     validUser: {
-      owner,
-      currency,
-      movements
+      currency
     }
   } = validateAndGetUser();
+  if (!isValid) return;
+
   const currencySign = currencies.get(currency);
+  renderUIComponent(labelDate, getCurrentDate());
+  initializeApp(validUser, currencySign);
 
-  if (isValid) {
-    containerApp.style.opacity = '100%';
-    setLogOutTimer();
-    renderWelcomeMessage(owner);
-    renderUIComponent(labelDate, getCurrentDate());
-    renderBalance(validUser, currencySign);
-    renderMovements([ ...movements ], currencySign);
-    renderSummary(validUser, currencySign);
-
-    sortMovementsBounded = sortMovements.bind(null, movements, currencySign);
-    transferMoneyBounded = transferMoney.bind(null, validUser, currencySign);
-    requestLoanBounded = requestLoan.bind(null, validUser, currencySign);
-    closeAccountBounded = closeAccount.bind(null, validUser);
-  }
+  sortMovementsBounded = sortMovements.bind(null, validUser, currencySign);
+  transferMoneyBounded = transferMoney.bind(null, validUser, currencySign);
+  requestLoanBounded = requestLoan.bind(null, validUser, currencySign);
+  closeAccountBounded = closeAccount.bind(null, validUser);
 });
 
-btnSort.addEventListener('click', sortMovementsBounded);
+btnSort.addEventListener('click', () => sortMovementsBounded());
 btnTransfer.addEventListener('click', event => transferMoneyBounded(event));
 btnLoan.addEventListener('click', event => requestLoanBounded(event));
 btnClose.addEventListener('click', event => closeAccountBounded(event));
